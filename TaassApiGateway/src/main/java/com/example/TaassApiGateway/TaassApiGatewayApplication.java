ce9371de02ad63eda209e9585d9f34c3034c6821
Gabriele Naretto <gabrielenaretto@gmail.com>
2021-12-14 18:31:49 +0100

    Initial Commit
    Commit iniziale con tutte le librerie di maven in qui si andra a implementare il file yalm per gestire l'api gateway

---
+++ TaassApiGateway/src/main/java/com/example/TaassApiGateway/TaassApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaassApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaassApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaassApiGatewayApplication.class, args);
+	}
+
+}
---
+++ TaassApiGateway/src/test/java/com/example/TaassApiGateway/TaassApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaassApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaassApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
