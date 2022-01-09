13e7c11e6447ebdb7ce44ceb8c74b93bcca60c6f
Livio Scarpinati <livio.scarpinati@gmail.com>
2022-01-09 20:31:25 +0100

    fix: added annotation for CORS policy
    added @CrossOrigin(origins = "*")

--- TaassApiGateway/src/main/java/com/example/TaassApiGateway/Controller/Controller.java
+++ TaassApiGateway/src/main/java/com/example/TaassApiGateway/Controller/Controller.java
@@ -4,10 +4,7 @@ import com.example.TaassApiGateway.Model.*;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -16,7 +13,7 @@ import java.util.ArrayList;
 //TODO metodo di delete
 
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/gateway")
 public class Controller {
